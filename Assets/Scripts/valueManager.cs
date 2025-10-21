d66e6b2fbead06913fb7295c58d69d727496de38
天人三策 <123095076+pymakertrsc@users.noreply.github.com>
2025-10-21 09:34:36 +0800

    42

---
+++ Assets/Scripts/valueManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+/*
+ * valueManager 收集游戏中的所有数值。
+ * 
+ * 数值脚本在启动时会访问该管理器并在此处注册，
+ * 无需手动链接。
+ */
+
+public class valueManager : MonoBehaviour
+{ 
+
+}
+
